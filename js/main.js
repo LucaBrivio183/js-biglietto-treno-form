12d3faf7490ee96df85313383609799af236e4b8
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-22 14:23:18 +0100

    feat: first  scaffolding

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//strict rules
+
+'use strict'
+
+//define a const variable for price∀km (for future changes)
+
+let standardKmPrice = 0.21;
+
+//create 2 input field in html, one for kms and  one for age
+
+
+//define a const variables for taking value  from html inputs
+
+
+//calc and define a  const variable for total price (price∀km * kms)
+
+
+//apply 20% discount for minors  //apply 40% discount for over65
+
+
+//print final price in console log
+
+
+
+
+
+
+
+
+
