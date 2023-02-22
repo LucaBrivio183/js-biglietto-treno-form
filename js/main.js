b8370a440a71924fd78c8e66bc8109bea0843a82
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-22 15:14:14 +0100

    feat: milestone  #1

--- js/main.js
+++ js/main.js
@@ -4,24 +4,30 @@
 
 //define a const variable for price∀km (for future changes)
 
-let standardKmPrice = 0.21;
-
-//create 2 input field in html, one for kms and  one for age
-
+const KmPrice = 0.21;
 
 //define a const variables for taking value  from html inputs
 
+const userKm = document.getElementById("input-km").value;
+const userAge = document.getElementById("input-age").value;
 
-//calc and define a  const variable for total price (price∀km * kms)
-
+//calc and define a let variable for total price (price∀km * kms)
+ 
+let price = KmPrice * userKm
 
 //apply 20% discount for minors  //apply 40% discount for over65
 
+if( userAge ===  'minor') {
+    price -= price * 0.2;
+    
+} else if ( userAge ===  'over65') {
+    price -= price * 0.4;
+    
+}
 
 //print final price in console log
 
-
-
+console.log(`${price}`);
 
 
 
