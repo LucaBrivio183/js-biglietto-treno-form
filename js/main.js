20e5647ffb1cdd5c9a5e6d234cc9dbd403825642
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-23 11:07:13 +0100

    feat: Milestone#2, hide ticket on reset

--- js/main.js
+++ js/main.js
@@ -8,35 +8,21 @@ const KmPrice = 0.21;
 
 //define a const variables for taking value  from html inputs
 
-const userKm = document.getElementById("input-km").value;
-const userAge = document.getElementById("input-age").value;
+const userKm = document.getElementById("input-km");
+const userAge = document.getElementById("input-age");
 
-//calc and define a let variable for total price (price∀km * kms)
- 
-let price = KmPrice * userKm
 
 //define a variable for showing discount on ticket
 
 let discountGroup = 'Biglietto standard';
 
-//apply 20% discount for minors  //apply 40% discount for over65
-
-if( userAge ===  'minor') {
-    price -= price * 0.2;
-    discountGroup = 'Sconto minorenni';
-    
-} else if ( userAge ===  'over65') {
-    price -= price * 0.4;
-    discountGroup = 'Sconto over 65';
-}
-
 //define a variable for generate button 
 
-const inputButton = document.getElementById('generate-btn');
+const inputGenerateButton = document.getElementById('generate-btn');
 
 //define a variable for user name from input
 
-const userName = document.getElementById('input-name').value;
+const userName = document.getElementById('input-name');
 
 //define a const variable for ticket item to change class on click
 
@@ -49,18 +35,43 @@ const CPcode = Math.floor(Math.random() * 100001) + Math.floor(Math.random() * 1
 
 //show details in ticket element after click
 
-inputButton.addEventListener('click', 
+inputGenerateButton.addEventListener('click', 
     
     function() {
-        document.getElementById('name').innerHTML = `${userName}`
-        document.getElementById('price').innerHTML = `${price.toFixed(2)} €`
-        ticket.classList.add('show');
-        document.getElementById('offer').innerHTML = `${discountGroup}`
+        //get current value at click
+        const userKmValue =  Number(userKm.value);
+        const userAgeValue =  userAge.value;
+        const userNameValue =  userName.value;
+        //calc price 
+        let price = KmPrice * userKmValue;
+        //apply correct discount 
+        if( userAgeValue ===  'minor') {
+            price -= price * 0.2;
+            discountGroup = 'Sconto minorenni';
+            
+        } else if ( userAgeValue ===  'over65') {
+            price -= price * 0.4;
+            discountGroup = 'Sconto over 65';
+        }
+
+        ticket.classList.add('show');//show ticket on reset
+        
+        //show value on ticket
+        document.getElementById('name').innerHTML = `${userNameValue}`;
+        document.getElementById('price').innerHTML = `${price.toFixed(2)} €`;
+        document.getElementById('offer').innerHTML = `${discountGroup}`;
         document.getElementById("carrozza").innerHTML = `${wagonNumber}`;
         document.getElementById("codicecp").innerHTML = `${CPcode}`;
-});
- 
+}
+);
 
+//define variable for reset  button
+const inputResetButton = document.getElementById('reset-btn');
 
+//add event on click of reset  button
+inputResetButton.addEventListener('click', 
 
-
+    function() {
+        ticket.classList.remove('show'); //hide ticket on reset
+}       
+);
